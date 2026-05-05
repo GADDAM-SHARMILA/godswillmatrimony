1e602a745b02940443037bdfdf70923ed1c81c17
gshar <gshar@SHARMILA>
2026-05-05 11:34:17 +0530

    Updated

--- src/main/java/com/godswill/matrimony/GodswillApplication.java
+++ src/main/java/com/godswill/matrimony/GodswillApplication.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-public class GodswillApplication {
+public class 	GodswillApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GodswillApplication.class, args);
